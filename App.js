a31f09d3d3238ba4c0a53d89d5dc23e6917d7136
Ganasa18 <ganasa18@yahoo.com>
2022-04-28 09:42:29 +0700

    setup firebase

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import firebase from "firebase/app";
+
+import Constants from "expo-constants";
+// Initialize Firebase
+if (firebase.apps.length == 0) {
+  firebase.initializeApp(Constants.manifest.web.config.firebase);
+}
 
 export default function App() {
   return (
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
